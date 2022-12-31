fdc5603dda453e242577c9af00fb43158bbae95d
viniciusgasparino <viniciusgasparino@yahoo.com.br>
2022-12-30 22:03:29 -0300

    Criado Container

--- pages/info/[lat]/[lon].jsx
+++ pages/info/[lat]/[lon].jsx
@@ -1,5 +1,6 @@
 import styled from "styled-components"
 import Navbar from "../../../src/components/navbar/Navbar"
+import Container from "../../../src/components/layout/container/Container"
 
 const Body = styled.div`
   background-color: ${props => props.theme.colors.grey};
@@ -11,7 +12,9 @@ function InfoPage(){
     <>
       <Navbar/>
       <Body>
-
+        <Container>
+          Conteudo
+        </Container>
       </Body>
     </>
   )
